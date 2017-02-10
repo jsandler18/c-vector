7e41d4c1ffb4ff097f409140c474d3ba14596400
Jake Sandler <jsandler18@gmail.com>
2017-02-10 13:36:13 -0500

    wrote insert and append

--- vec.c
+++ vec.c
@@ -4,7 +4,27 @@
 
 #define MIN_SIZE 64
 
-int grow(vec_t * vector);
+int grow(vec_t * vector) {
+    void * tmp = realloc(vector->array, vector->allocated_slots * 2 * vector->element_size);
+    if (tmp == NULL) 
+        return VEC_COULD_NOT_ALLOCATE_MEMORY;
+
+    vector->array = tmp;
+    vector->allocated_slots *= 2;
+
+    return VEC_SUCCESS;
+}
+
+int shrink(vec_t * vector) {
+    void * tmp = realloc(vector->array, vector->allocated_slots * vector->element_size / 2);
+    if (tmp == NULL) 
+        return VEC_COULD_NOT_ALLOCATE_MEMORY;
+
+    vector->array = tmp;
+    vector->allocated_slots /= 2;
+
+    return VEC_SUCCESS;
+}
 
 int init (vec_t * vector, size_t element_size) {
     vector->element_size = element_size;
@@ -23,8 +43,31 @@ int init (vec_t * vector, size_t element_size) {
     //everything is good
     return VEC_SUCCESS;
 }
-int insert(vec_t * vector, void * element_ptr, int idx);
-int append(vec_t * vector, void * element_ptr); 
+int insert(vec_t * vector, void * element_ptr, int idx) {
+    //check that have enough space, grow if necesary
+    if (vector->used_slots == vector->allocated_slots) {
+        if (!grow(vector)) 
+            return VEC_COULD_NOT_ALLOCATE_MEMORY;
+    }
+
+    //check bounds
+    if (!(idx >=0 && idx <= vector->used_slots)) 
+        return VEC_INDEX_OUT_OF_BOUNDS;
+    
+    //move everything over one
+    memcpy(vector->array + (idx + 1) * vector->element_size,
+            vector->array + (idx) * vector->element_size,
+            (vector->used_slots - idx) * vector->element_size);
+
+    //insert
+    memcpy(vector->array + idx * vector->element_size, element_ptr, vector->element_size);
+
+    return VEC_SUCCESS;
+
+}
+int append(vec_t * vector, void * element_ptr) {
+    return insert(vector, element_ptr, vector->used_slots);
+}
 int veclen(vec_t * vector);
 int remove_element(vec_t * vector, void * element_ptr);
 int remove_index(vec_t * vector, int idx);
